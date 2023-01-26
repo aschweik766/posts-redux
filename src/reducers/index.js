aa0876de3fa9b50d637299e22a094014eafe3842
Aschweik766 <ashnjackson766@gmail.com>
2023-01-25 18:26:41 -0700

    add form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
+import { applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+const composeEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
+
+// const store = createStore(rootReducer, composeEnhancer);
 
 const store = configureStore({
     reducer: rootReducer,
+    middleware: thunk,
   });
 
 export default store;
