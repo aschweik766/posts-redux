571cd8aaf2de12b09b9c7bf8ffebff6b84500409
Aschweik766 <ashnjackson766@gmail.com>
2023-01-30 19:06:23 -0700

    slice mods

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,18 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
-import { applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
 import thunk from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from '@redux-devtools/extension';
 
-const composeEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
+// const composeEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
 
 // const store = createStore(rootReducer, composeEnhancer);
 
+// const store = configureStore({
+//     reducer: rootReducer,
+//     middleware: (getDefaultMiddleware),
+//   });
+
+
 const store = configureStore({
-    reducer: rootReducer,
-    middleware: thunk,
-  });
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunkMiddleware),
+  devTools: process.env.NODE_ENV !== 'production',
+
+})
+
+// Store has all of the default middleware added, _plus_ the logger middleware
 
 export default store;
 
